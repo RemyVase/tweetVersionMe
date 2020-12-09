d9575324c718a83bb2f9c2358338f30c977e4ba1
RemyVase <44263729+RemyVase@users.noreply.github.com>
2020-12-09 10:48:19 +0100

    Connexion database

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,28 @@
 const express = require('express');
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'tweetly'
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+const getAllUsers = connection.query('CALL getAllUsers', (error, results, fields) => {
+    if(error){
+        return console.error(error.message);
+    }
+    console.log(results[0]);
+})
+
 
 const app = express();
 
